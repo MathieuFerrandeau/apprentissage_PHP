82e87dd9bbec3975aa92e7105f172483ad570916
Mathieu Ferrandeau <m.ferrandeau@hotmail.fr>
2018-07-18 22:23:55 +0200

    Ajout des différentes formes de boucles dans le fichier boucles.php

---
+++ Boucles/boucles.php
@@ -0,0 +1,15 @@
+<?php
+
+
+$repetitions = 0;
+
+while ($repetitions <= 10) {
+  echo 'je suis à ' . $repetitions . '.';
+  $repetitions++;
+}
+
+for ($i=0 ; $i < 10 ; $i++ ) {
+  echo 'test' . $i . '.';
+}
+
+ ?>
