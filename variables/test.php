1157d503d57fca68739283b46f1ebaaea80eb2d0
Mathieu Ferrandeau <m.ferrandeau@hotmail.fr>
2018-07-18 21:52:34 +0200

    Mise au point sur les variables

---
+++ variables/test.php
@@ -0,0 +1,25 @@
+<?php
+
+$ageDuVisiteur = 20;
+$monNom ="Mathieu";
+$chiffreDécimal = 26.3;
+$boleen = false;
+
+echo  "Le visiteur à $ageDuVisiteur ans";
+echo "Je m'apelle $monNom";
+echo 'Mon chiffre décimal : ' . $chiffreDécimal . ' .';
+echo $boleen;
+
+$nombre = 2 + 4; // $nombre prend la valeur 6
+$nombre = 5 - 1; // $nombre prend la valeur 4
+$nombre = 3 * 5; // $nombre prend la valeur 15
+$nombre = 10 / 2; // $nombre prend la valeur 5
+
+// Allez on rajoute un peu de difficulté
+$nombre = 3 * 5 + 1; // $nombre prend la valeur 16
+$nombre = (1 + 2) * 2; // $nombre prend la valeur 6
+$nombre = 10;
+$resultat = ($nombre + 5) * $nombre; // $resultat prend la valeur 150
+
+
+ ?>
