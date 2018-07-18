69af2a9183c314dc97961fe4ecf3e00677f737f9
Mathieu Ferrandeau <m.ferrandeau@hotmail.fr>
2018-07-18 21:28:25 +0200

    création dun fichier.php pour commencer les test

---
+++ cours1/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Page de test</title>
+  </head>
+  <body>
+    <h2>Cours 1 PHP</h2>
+    <p>
+      Une page contenant du HTML avec des balises PHP<br>
+      Nous allons effectuer des petits test.
+      <?php /* Ceci est un commentaire PHP */?>
+      Une liste ci-dessous:
+
+    </p>
+    <ul>
+      <li style= "color: blue;">Texte en bleue</li>
+      <li style= "color: red;">Texte en rouge</li>
+      <li style= "color: green;">Texte en vert</li>
+    </ul>
+  </body>
+</html>
