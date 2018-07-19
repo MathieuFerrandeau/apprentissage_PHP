791dd4a461a5c06ca1f9b1737b0b217a4409973e
Mathieu Ferrandeau <m.ferrandeau@hotmail.fr>
2018-07-19 22:16:02 +0200

    Les portions de pages : voir qu'il est possible d'extraire un menu par exemple et de le rattacher à plusieurs pages php en ayant à en modifier qu'une

---
+++ portions_de_page/menu.php
@@ -0,0 +1,12 @@
+<!-- Le menu -->
+
+<nav id="menu">
+    <div class="element_menu">
+        <h3>Titre menu</h3>
+        <ul>
+            <li><a href="portionsDePage.php">Lien</a></li>
+            <li><a href="portionsDePage2.php">Lien</a></li>
+            <li><a href="page3.html">Lien</a></li>
+        </ul>
+    </div>
+</nav>
---
+++ portions_de_page/portionsDePage2.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Mon super site</title>
+    </head>
+
+    <body>
+
+    <!-- L'en-tête -->
+<?php  include("entete.php") ?>
+
+  <?php include("menu.php");   ?>
+
+    <!-- Le corps -->
+
+    <div id="corps">
+        <h1>Mon super site</h1>
+
+        <p>
+            Bienvenue sur mon super site !<br />
+            Ceci est la deuxième page
+        </p>
+    </div>
+
+    <!-- Le pied de page -->
+
+    <?php include("piedDePage.php");   ?>
+
+
+    </body>
+</html>
