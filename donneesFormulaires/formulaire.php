0d8f259122061097bcf48cebf93afd940c7f971c
Mathieu Ferrandeau <m.ferrandeau@hotmail.fr>
2018-07-21 19:54:54 +0200

    chapitre : envoie de données via les form

---
+++ donneesFormulaires/formulaire.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <p>
+    Cette page ne contient que du HTML.<br />
+    Veuillez taper votre prénom :
+</p>
+
+<form action="cible.php" method="post">
+<p>
+    <input type="text" name="prenom" />
+    <input type="submit" value="Valider" />
+</p>
+</form>
+
+  </body>
+</html>
