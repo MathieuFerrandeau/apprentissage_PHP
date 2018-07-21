313f8c958a9caf4000479182c0cc851dc165213e
Mathieu Ferrandeau <m.ferrandeau@hotmail.fr>
2018-07-21 19:28:22 +0200

    chapitre :transmettre des données dans l'url

---
+++ donneesURL/donneesURL.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8"/>
+    <title>MON SITE</title>
+  </head>
+  <body>
+    <p>
+        <a href="bonjour.php?nom=Ferrandeau&amp;prenom=Mathieu&repeter=1">Dis-moi bonjour !</a>
+    </p>
+  </body>
+</html>
