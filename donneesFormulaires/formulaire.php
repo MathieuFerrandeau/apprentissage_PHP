ebcb7b656ae7a902fc442a78ccdacbebc9661ce2
Mathieu Ferrandeau <m.ferrandeau@hotmail.fr>
2018-07-21 19:58:38 +0200

    ajout d'un form pour l'envoi de fichier

--- donneesFormulaires/formulaire.php
+++ donneesFormulaires/formulaire.php
@@ -16,6 +16,13 @@
     <input type="submit" value="Valider" />
 </p>
 </form>
+<form action="cible_envoi.php" method="post" enctype="multipart/form-data">
+        <p>
+                Formulaire d'envoi de fichier :<br />
+                <input type="file" name="monfichier" /><br />
+                <input type="submit" value="Envoyer le fichier" />
+        </p>
+</form>
 
   </body>
 </html>
